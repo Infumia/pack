59a56684628b1d487715db8dff7497e600e6f5b6
Hasan Demirtaş <utsukushihito@outlook.com>
2024-06-23 21:49:59 +0300

    Update ResourceProducers.java

--- common/src/main/java/net/infumia/pack/ResourceProducers.java
+++ common/src/main/java/net/infumia/pack/ResourceProducers.java
@@ -1,10 +1,11 @@
 package net.infumia.pack;
 
-import java.util.List;
 import net.kyori.adventure.key.Key;
 import team.unnamed.creative.base.Writable;
 import team.unnamed.creative.font.Font;
 import team.unnamed.creative.texture.Texture;
+
+import java.util.List;
 
 /**
  * Utility class for creating instances of {@link ResourceProducer}.
@@ -62,23 +63,23 @@ public final class ResourceProducers {
     /**
      * Creates a {@link GlyphImage} instance with the specified key, texture, and properties.
      *
-     * @param key        The key associated with the glyph image. Cannot be null.
+     * @param fontKey    The font key associated with the glyph image. Cannot be null.
      * @param texture    The texture of the glyph image. Cannot be null.
      * @param properties The properties of the glyph image. Cannot be null.
      * @return A {@link GlyphImage} instance.
      */
     public static GlyphImage image(
-        final Key key,
+        final Key fontKey,
         final Texture texture,
         final TextureProperties properties
     ) {
-        return new GlyphImageImpl(key, texture, properties);
+        return new GlyphImageImpl(fontKey, texture, properties);
     }
 
     /**
      * Creates a {@link ResourceProducerImageMultichar} instance with the specified parameters.
      *
-     * @param fontKey           The key associated with the font. Cannot be null.
+     * @param fontKey           The font key associated with the font. Cannot be null.
      * @param texture           The texture of the glyph collection. Cannot be null.
      * @param properties        The properties of the texture. Cannot be null.
      * @param charactersMapping The list of character mappings for the glyphs. Cannot be null.
