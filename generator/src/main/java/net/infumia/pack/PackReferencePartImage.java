681986d135968b19d2597276e69dca21725403ff
Hasan Demirtaş <utsukushihito@outlook.com>
2024-06-25 17:23:09 +0300

    Update PackReferencePartImage.java

--- generator/src/main/java/net/infumia/pack/PackReferencePartImage.java
+++ generator/src/main/java/net/infumia/pack/PackReferencePartImage.java
@@ -4,11 +4,15 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.nio.file.Path;
 import java.util.Locale;
+import java.util.StringJoiner;
 import net.kyori.adventure.key.Key;
 import team.unnamed.creative.base.Writable;
 import team.unnamed.creative.font.Font;
 import team.unnamed.creative.texture.Texture;
 
+/**
+ * Represents an image part of a pack reference.
+ */
 public final class PackReferencePartImage extends PackReferencePart {
 
     private final String namespace;
@@ -19,13 +23,22 @@ public final class PackReferencePartImage extends PackReferencePart {
 
     private Path directory;
 
+    /**
+     * Ctor.
+     *
+     * @param namespace the namespace of the image. Can be null
+     * @param key       the key for the image. Cannot be null.
+     * @param image     the image file name. Cannot be null.
+     * @param height    the height of the image.
+     * @param ascent    the ascent value of the image.
+     */
     @JsonCreator
     public PackReferencePartImage(
         @JsonProperty("namespace") final String namespace,
-        @JsonProperty("key") final String key,
-        @JsonProperty("image") final String image,
-        @JsonProperty("height") final int height,
-        @JsonProperty("ascent") final int ascent
+        @JsonProperty(value = "key", required = true) final String key,
+        @JsonProperty(value = "image", required = true) final String image,
+        @JsonProperty(value = "height", required = true) final int height,
+        @JsonProperty(value = "ascent", required = true) final int ascent
     ) {
         this.namespace = namespace;
         this.key = key;
@@ -77,5 +90,17 @@ public final class PackReferencePartImage extends PackReferencePart {
     PackReferencePartImage directory(final Path directory) {
         this.directory = directory;
         return this;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", PackReferencePartImage.class.getSimpleName() + "[", "]")
+            .add("namespace='" + this.namespace + "'")
+            .add("key='" + this.key + "'")
+            .add("image='" + this.image + "'")
+            .add("height=" + this.height)
+            .add("ascent=" + this.ascent)
+            .add("directory=" + this.directory)
+            .toString();
     }
 }
