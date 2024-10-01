28e810d14363fd7091cbdab33535acb2e37122bc
Hasan Demirtaş <utsukushihito@outlook.com>
2024-10-01 18:33:31 +0300

    configurable blank slot custom model data.

--- blank/src/main/java/net/infumia/pack/BlankSlot.java
+++ blank/src/main/java/net/infumia/pack/BlankSlot.java
@@ -1,8 +1,6 @@
 package net.infumia.pack;
 
 import net.kyori.adventure.key.Key;
-import net.kyori.adventure.key.KeyPattern;
-import team.unnamed.creative.base.Writable;
 
 /**
  * Utility class for creating and retrieving blank slot file resources.
@@ -12,52 +10,20 @@ public final class BlankSlot {
     /**
      * Retrieves a pre-configured blank slot file resource.
      *
+     * @param customModelData the custom model data of the paper model.
+     *
      * @return A {@link FileResource} representing the blank slot.
      */
-    public static FileResource get() {
-        return Internal0.BLANK_SLOT.get();
-    }
-
-    /**
-     * Creates a blank slot file resource with the specified parameters.
-     *
-     * @param namespace       The namespace for the model. Cannot be null.
-     * @param itemId          The id for the item. Cannot be null.
-     * @param baseKey         The key for the base model. Cannot be null.
-     * @param blankSlotImage  The writable image for the blank slot. Cannot be null.
-     * @param customModelData The custom model data value.
-     * @return A {@link FileResource} representing the created blank slot.
-     */
-    public static FileResource create(
-        @KeyPattern.Namespace final String namespace,
-        @KeyPattern.Value final String itemId,
-        final Key baseKey,
-        final Writable blankSlotImage,
-        final int customModelData
-    ) {
+    public static FileResource get(final int customModelData) {
         return ResourceProducers.item(
-            Key.key(namespace, itemId),
-            baseKey,
-            blankSlotImage,
+            Key.key(Internal.DEFAULT_NAMESPACE, "blank_slot"),
+            Key.key("item/paper"),
+            Internal.resourceFromJar("blank_slot.png"),
             customModelData
         );
     }
 
     private BlankSlot() {
         throw new IllegalStateException("Utility class");
-    }
-
-    private static final class Internal0 {
-
-        private static final Lazy<FileResource> BLANK_SLOT = Lazy.of(
-            () ->
-                BlankSlot.create(
-                    Internal.DEFAULT_NAMESPACE,
-                    "blank_slot",
-                    Key.key("item/paper"),
-                    Internal.resourceFromJar("blank_slot.png"),
-                    1
-                )
-        );
     }
 }
